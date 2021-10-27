c989bcce08e8a1b305d228a5af7633f06a6fe7de
Mikhail Sizov <mssizov@gmail.com>
2021-10-27 23:29:10 +0300

    Power and dot. Order change

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-const labels=['+','-','*','/','1','2','3','4','5','6','7','8','9','0','=']
-const keyCodes=['+','-','*','/','=','1','2','3','4','5','6','7','8','9','0','Enter', 'Backspace','Delete','Tab','.']
+const labels=['1','2','3','⌫','AC','4','5','6','+','-','7','8','9','*','/','0','.','=','^']
+const keyCodes=['+','-','*','/','^','=','1','2','3','4','5','6','7','8','9','0','Enter', 'Backspace','Delete','Tab','.']
 
 class CalcButton extends React.Component {
   render() {
@@ -22,7 +22,7 @@ class CalculatorView extends React.Component{
     this.inputEnter = this.inputEnter.bind(this)
     this.clear = this.clear.bind(this)
     this.state={calc:0,value:'', result:'0',op:'=',
-    funcs:{'+':(a,b)=>{return a+b},'-':(a,b)=>{return a-b},'*':(a,b)=>{return a*b},'/':(a,b)=>{return a/b},'=':(a,b)=>{return b}}}
+    funcs:{'+':(a,b)=>{return a+b},'-':(a,b)=>{return a-b},'*':(a,b)=>{return a*b},'/':(a,b)=>{return a/b}, '^':(a,b)=>{return a**b},'=':(a,b)=>{return b}}}
   }
 
   render() {
@@ -31,7 +31,6 @@ class CalculatorView extends React.Component{
         <div className="result" id="result" >{this.state.result}</div>  
         <input type="text" id="inp"ref={(el) => { this.inp = el;}}  value={this.state.value} onKeyDown={this.inputEnter} onChange={this.inputChange} placeholder="0"/>
         <div className="buttons">
-          <CalcButton value='AC'  onClick={this.clear}/>
           {labels.map((item) => <CalcButton value={item}  onClick={this.clickHandler}/>)}
         </div>
       </form>
@@ -43,15 +42,28 @@ class CalculatorView extends React.Component{
   }
 
   click(operation){
-    if(operation.toString().match(/[0-9.]/)!==null)
-      this.setState({value:this.state.value+operation})
-    else{
-      this.update()
-      this.setState({
-        op:operation,
-        value:""},()=>{this.setState({result:this.state.calc.toString()+(operation==='='?'':operation)})})
-    }
     this.inp.focus()
+    if(operation.toString().match(/[0-9]/)!==null){
+      this.setState({value:this.state.value+operation})
+      return
+    }
+    if(operation==='.'){
+      if(this.state.value.toString().indexOf('.')===-1)
+        this.setState({value:this.state.value+'.'})
+      return
+    }
+    if(operation==='AC'){
+      this.clear()
+      return
+    }
+    if(operation==='⌫'){
+      this.deleteLast();
+      return
+    }
+    this.update()
+    this.setState({
+      op:operation,
+      value:""},()=>{this.setState({result:this.state.calc.toString()+(operation==='='?'':operation)})})
   }
 
   update(){
@@ -72,7 +84,7 @@ class CalculatorView extends React.Component{
     }
     if(keyCode === 'Backspace'){
       e.preventDefault();
-      this.setState({value:this.state.value.substr(0,this.state.value.length-1)})
+      this.deleteLast();
     }
     if(keyCode === 'Delete'){
       e.preventDefault();
@@ -93,10 +105,14 @@ class CalculatorView extends React.Component{
       this.setState({value:value.substr(0,value.length-1)})
     else
       this.setState({value:value})
-    if(lastChar.toString().match(/[/*+-=]/)!==null)
+    if(lastChar.toString().match(/[/*+-=^]/)!==null)
       this.click(lastChar)
     if(lastChar==='.' && this.state.value.toString().indexOf('.')!==-1)
       this.setState({value:value.substr(0,value.length-1)})
+  }
+
+  deleteLast(){
+    this.setState({value:this.state.value.substr(0,this.state.value.length-1)})
   }
 }
 
